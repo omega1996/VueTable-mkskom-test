fc2e85a232317108e44fda1fd72b861ae91c8360
IvanR <yu181219710>
2021-07-12 12:13:07 +0500

    initial commit

---
+++ item-period/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+
+      <v-spacer></v-spacer>
+      <v-tabs>
+        <v-tab>Items</v-tab>
+        <v-tab>Periods</v-tab>
+        <v-tab>Dashboard</v-tab>
+      </v-tabs>
+
+    </v-app-bar>
+
+    <v-main>
+
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
